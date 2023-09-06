7bf6afb746bce4c94fa00656efcdd07ebb2654c3
Ana Lívia Magalhães Garbin <analiviamagalhaesg@gmail.com>
2023-09-06 09:10:10 -0300

    lacos blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,34 +2,22 @@
 
 @php 
     //codigo em php
-    //condicional ternário
-    //if($statusF=='N') ? 'Está inativo.' : 'Esta ativo.'
 @endphp
 
 {{-- Estruturas no blade --}}
 
-{{-- If --}}
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados.</h3>
-@elseif(count($fornecedores)>10)
-    <h3>Existem vários fornecedores cadastrados.</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados.</h3>
-@endif
-
-{{-- Isset (verifica se a variavel está definida) --}}
 @isset($fornecedores)
 
-    Fornecedor: {{ $nomeF = $fornecedores[0]['nome'] }}
-    <br />
-    Status: {{ $statusF = $fornecedores[0]['status']}}
-
-    {{-- Unless (contrário do if) --}}
-    @Unless($statusF=='S')
-        <h3>O fornecedor de nome {{ $nomeF }} está inativo.</h3>
-    @endif
+    @foreach($fornecedores as $indice => $fornecedor)
+        <br />
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br />
+        Status: {{ $fornecedor['status'] }}
+        <br />
+        CNPJ: {{ $fornecedor['cnpj'] ?? '' }}
+        <br />
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <hr>
+    @endforeach
 
 @endisset
-
-{{-- empty (valores vazios) --}}
-{{-- default '??' ---}}
